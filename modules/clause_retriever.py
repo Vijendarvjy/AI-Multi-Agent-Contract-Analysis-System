5f7321583fdd8d17ded5c0f03ea54602a9509a0b
Vijendarvjy <vijendareslavath19@gmail.com>
2026-04-27 16:05:03 +0530

    Add clause splitting function using RecursiveCharacterTextSplitter

--- modules/clause_retriever.py
+++ modules/clause_retriever.py
@@ -1,1 +1,9 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
+
+def split_clauses(text: str):
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+    return splitter.split_text(text)
